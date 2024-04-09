df06f663195b2296dc0dab37458b359bb4d2a8a9
Ganesh Prasad Sahoo <ganeshprasadsahoo348@gmail.com>
2024-04-09 11:45:35 +0530

    files added

---
+++ index.js
@@ -0,0 +1,18 @@
+
+// Accordian
+
+document.addEventListener("DOMContentLoaded", function() {
+  var btn = document.getElementById("btn");
+
+  btn.addEventListener("click", function() {
+    var educationAccordionButton = document.querySelector("#headingOne button");
+
+    educationAccordionButton.click();
+  });
+});
+
+
+
+
+
+
